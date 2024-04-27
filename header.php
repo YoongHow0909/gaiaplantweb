ef3f45052fa0b896343c9cce092fbb27af4b26e5
YoongHow0909 <168264618+YoongHow0909@users.noreply.github.com>
2024-04-27 16:27:04 +0800

    Create header.php

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html>
+    <link href="gaia_css/header.css" rel="stylesheet" type="text/css"/>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        
+    </head>
+    <header>
+        <div class="head_design">
+        <img class="logo" src="image/logo.png" onclick="window.location.href = 'aboutUs.php';">  
+        <p class ="title">Gaia's <br>Garden</p> 
+        
+        <div class="btn_row">
+            <button class="head_btn" onclick="window.location.href = 'home.php';"><span>Home </span></button>
+            <button class="head_btn" onclick="window.location.href = 'aboutUs.php';"><span>About us</span></button>
+        <button class="head_btn" onclick="window.location.href = 'plant_menu.php';"><span>Shop</span></button>
+        <button class="head_btn" onclick="window.location.href = 'contactUs.php';"><span>Contact us</span></button>
+        <button class="Sign_in">Get Started</button>
+        </div>
+        
+        
+        </div>
+    </header>
+    <body>
+         
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
