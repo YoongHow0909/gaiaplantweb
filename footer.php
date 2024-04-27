8bf67dfa30a349e5eff70c306861f03438203f05
YoongHow0909 <168264618+YoongHow0909@users.noreply.github.com>
2024-04-27 16:26:40 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="gaia_css/footers.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <div class="footer_page">
+            <style>
+                .fa{
+                    display: inline-table;
+                }
+            </style>
+        <a href="https://www.facebook.com/yonghow.lin/" class="fa fa-facebook"></a>
+        <a href="https://www.instagram.com/yoonghow04/" class="fa fa-instagram"></a>
+        <a href="#" class="fa fa-twitter"></a>
+        <a href="https://www.youtube.com/channel/UCNZ9eRTbn35ey08N8IY2Dpw" class="fa fa-youtube"></a>
+        <a href="https://www.linkedin.com/in/yoong-how-lim-a48a94284/" class="fa fa-linkedin"></a>
+        <br>
+        <br>
+
+        <a href="home.php">Home</a><a href="aboutUs.php">About Us</a><a href="plant_menu.php">Shop</a><a href="contactUs.php">Contact Us</a>
+          <br>
+            <br>
+
+        <p >Copyright &copy2024, design by Gaia Team</p>
+        </div>
+    </body>
+    <footer>
+        
+    </footer>
+</html>
