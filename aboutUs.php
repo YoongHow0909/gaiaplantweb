2d31d03e255c48bd370f948fa145b00f7718caea
YoongHow0909 <168264618+YoongHow0909@users.noreply.github.com>
2024-04-27 16:26:32 +0800

    Create aboutUs.php

---
+++ aboutUs.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>aboutUs</title>
+        <link href="gaia_css/aboutUs.css" rel="stylesheet" type="text/css"/>
+        <?php
+    include ('header.php');
+?>
+    </head>
+    <body>
+
+<?php
+include('sidebar.php');
+
+?>
+<br>
+<h1>About Us</h1>
+<br>
+<br>
+<article>
+<p>
+<b>Welcome to Gaia's Garden, where nature's beauty meets online convenience!</b>
+<br><br>
+<b>At Gaia's Garden</b>, we're passionate about bringing the wonders of nature directly to your doorstep. 
+Our online platform is your gateway to a lush world of greenery, offering an extensive selection of plants to 
+enrich your living spaces, enhance your well-being, and reconnect you with the tranquility of the natural world.
+<br><br>
+With a commitment to quality and sustainability, we curate a diverse range of plants, 
+from vibrant succulents to majestic ferns, and everything in between. Whether you're an avid gardener, 
+a plant enthusiast, or a beginner looking to add a touch of green to your home or office, Gaia's Garden has something for everyone.
+</p>
+
+<img src="image/logo.png" alt="logo" class="about_logo">
+</article>
+
+<article >
+
+<p class="right_side_about_us">
+Our user-friendly website makes browsing and purchasing a breeze, providing detailed descriptions and 
+care instructions for each plant to ensure your gardening journey is as rewarding as it is effortless. 
+Plus, with convenient delivery options, you can enjoy the convenience of shopping from the comfort of your own home, while we handle the rest.
+<br><br>
+Join us in cultivating beauty, sustainability, and a deeper connection to the natural world , 
+welcome to Gaia's Garden, where your green dreams come to life!</p>
+
+</article>
+<img src="image/about_us_plant.jpg" alt="about_us_plant" class="about_logo2">
+
+<article>
+
+<p>
+Meet the passionate minds behind Gaia's Garden , a team dedicated to nurturing your love for plants 
+and bringing nature's beauty into your life.
+<br><br>
+Our team comprises a diverse group of plant enthusiasts, horticulturists, designers, and customer service experts,
+ all united by a shared love for greenery and a commitment to providing exceptional service to our customers.
+</p>
+<img src="image/team-dynamics.jpg" alt="team" class="team_show">
+
+
+</article>
+
+
+    </body>
+    <footer>
+<?php
+include('footer.php');
+?>
+    </footer>
+</html>
